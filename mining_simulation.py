b4891b2725bfa9d1c2e3e2c4708a39cf6d12845a
AmberChaoYang <chaoyang@seas.upenn.edu>
2021-03-13 20:03:12 -0500

    Update mining_simulation.py
    deleted the testing part per the hw description

--- mining_simulation.py
+++ mining_simulation.py
@@ -108,22 +108,5 @@ def Simulate(alpha,gamma,N, seed):
 # In[26]:
 
 
-""" 
-  Uncomment out the following lines to try out your code
-  DON'T include it in your final submission though.
-"""
-#let's run the code with the follwing parameters!
-alpha=0.35
-gamma=0.5
-Nsimu=10**7
-seed = 100
-#This is the theoretical probability computed in the original paper
-print("Theoretical probability :",(alpha*(1-alpha)**2*(4*alpha+gamma*(1-2*alpha))-alpha**3)/(1-alpha*(1+(2-alpha)*alpha)))
-print("Simulated probability :",Simulate(alpha,gamma,Nsimu, seed))
-
-
-# In[ ]:
-
-
 
 
